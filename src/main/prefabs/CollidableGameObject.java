13033bb50c2525dc0c0d64e75ce13a9fcc952c06
Ivan <ivandorofeev@icloud.com>
2019-02-01 16:48:40 +0100

    Continue doesCollide

--- src/main/prefabs/CollidableGameObject.java
+++ src/main/prefabs/CollidableGameObject.java
@@ -2,10 +2,8 @@ package main.prefabs;
 
 import java.awt.*;
 
-import main.util.BoxCollider;
-import main.util.CollisionState2D;
+import main.util.*;
 import main.util.Rectangle;
-import main.util.Vector;
 
 public class CollidableGameObject {
 	
@@ -20,39 +18,39 @@ public class CollidableGameObject {
 		this.polygon = polygon;
 		this.color = color;
 	}
-
+	
 	public BoxCollider getCollider() {
 		return collider;
 	}
-
+	
 	public void setCollider(BoxCollider collider) {
 		this.collider = collider;
 	}
-
+	
 	public Vector getSpeed() {
 		return speed;
 	}
-
+	
 	public void setSpeed(Vector speed) {
 		this.speed = speed;
 	}
-
+	
 	public Polygon getPolygon() {
 		return polygon;
 	}
-
+	
 	public void setPolygon(Polygon polygon) {
 		this.polygon = polygon;
 	}
-
+	
 	public Color getColor() {
 		return color;
 	}
-
+	
 	public void setColor(Color color) {
 		this.color = color;
 	}
-
+	
 	public void applySpeed() {
 		translate(speed);
 	}
@@ -65,8 +63,8 @@ public class CollidableGameObject {
 	}
 	
 	public void setPosition(Vector v) {
-		int deltaX = (int)(v.getX() - collider.getPosition().getX());
-		int deltaY = (int)(v.getY() - collider.getPosition().getY());
+		int deltaX = (int) (v.getX() - collider.getPosition().getX());
+		int deltaY = (int) (v.getY() - collider.getPosition().getY());
 		polygon.translate(deltaX, deltaY);
 		collider.setPosition(v);
 	}
@@ -76,28 +74,61 @@ public class CollidableGameObject {
 	}
 	
 	public CollisionState2D doesCollide(CollidableGameObject collideWith) {
+		CollisionState collisionVer = CollisionState.NONE;
+		CollisionState collisionHor = CollisionState.NONE;
+		
 		// todo
-		if(speed.getX() != 0){
-			doesCollideHor(collideWith);
+		if (collider.getPosition().getX() > collideWith.getPosition().getX()) {
+			if (doesCollideHor(this, collideWith)) {
+				collisionHor = CollisionState.LEFT;
+			}
+		} else {
+			if (doesCollideHor(collideWith, this)) {
+				collisionHor = CollisionState.RIGHT;
+			}
 		}
-		if(speed.getY() != 0){
-			doesCollideVer(collideWith);
+		
+		if (collider.getPosition().getY() > collideWith.getPosition().getY()) {
+			if (doesCollideVer(this, collideWith)) {
+				collisionVer = CollisionState.TOP;
+			}
+		} else {
+			if (doesCollideVer(collideWith, this)) {
+				collisionVer = CollisionState.BOTTOM;
+			}
 		}
 		
 		
-		return false;
+		return new CollisionState2D(collisionHor, collisionVer);
 	}
 	
-	private boolean doesCollideHor(CollidableGameObject left, CollidableGameObject right){
-		if (){
+	
+	private boolean doesCollideHor(CollidableGameObject left, CollidableGameObject right) {
 		
+		SectionVertical sectionLeft = new SectionVertical(left.getCollider().getRight() + left.getSpeed().getX(),
+				left.getCollider().getTop() + left.getSpeed().getY(), left.getCollider().getBottom() + left.getSpeed().getY());
+		SectionVertical sectionRight = new SectionVertical(right.getCollider().getLeft() + right.getSpeed().getX(),
+				right.getCollider().getTop() + right.getSpeed().getY(), right.getCollider().getBottom() + right.getSpeed().getY());
+		
+		
+		if (sectionLeft.getBottomY() > sectionRight.getTopY() || sectionLeft.getTopY() < sectionRight.getBottomY()) {
+			return false;
 		}
-		return false;
+		return true;
 	}
 	
-	private boolean doesCollideVer(CollidableGameObject top, CollidableGameObject bottom){
+	private boolean doesCollideVer(CollidableGameObject top, CollidableGameObject bottom) {
 		
-		return null;
+		SectionHorizontal sectionTop = new SectionHorizontal(top.getCollider().getBottom() + top.getSpeed().getY(),
+				top.getCollider().getRight() + top.getSpeed().getX(), top.getCollider().getLeft() + top.getSpeed().getX());
+		SectionHorizontal sectionBottom = new SectionHorizontal(bottom.getCollider().getTop() + bottom.getSpeed().getY(),
+				bottom.getCollider().getRight() + bottom.getSpeed().getX(), bottom.getCollider().getLeft() + bottom.getSpeed().getX());
+		
+		if (top.getCollider().getRight() < bottom.getCollider().getLeft() || top.getCollider().getLeft() > bottom.getCollider().getRight()) {
+			return false;
+		}
+		
+		return true;
 	}
-
+	
 }
