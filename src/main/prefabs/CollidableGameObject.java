6e8deef369c30ece9407281f851cdbd951f3c0c3
Andrey <fastrazor2000@gmail.com>
2019-02-01 14:17:23 +0300

    Add CollidableGameObject

---
+++ src/main/prefabs/CollidableGameObject.java
@@ -0,0 +1,79 @@
+package main.prefabs;
+
+import java.awt.*;
+
+import main.util.BoxCollider;
+import main.util.Rectangle;
+import main.util.Vector;
+
+public class CollidableGameObject {
+	
+	private BoxCollider collider;
+	private Vector speed;
+	private Polygon polygon;
+	private Color color;
+	
+	public CollidableGameObject(Vector pos, double colliderWidth, double colliderHeight, Vector speed, Polygon polygon, Color color) {
+		collider = new BoxCollider(pos, colliderWidth, colliderHeight);
+		this.speed = speed;
+		this.polygon = polygon;
+		this.color = color;
+	}
+
+	public BoxCollider getCollider() {
+		return collider;
+	}
+
+	public void setCollider(BoxCollider collider) {
+		this.collider = collider;
+	}
+
+	public Vector getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Vector speed) {
+		this.speed = speed;
+	}
+
+	public Polygon getPolygon() {
+		return polygon;
+	}
+
+	public void setPolygon(Polygon polygon) {
+		this.polygon = polygon;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public void applySpeed() {
+		applyCustomSpeed(speed);
+	}
+	
+	public void applyCustomSpeed(Vector speed) {
+		Vector pos = Vector.clone(getPosition());
+		pos.setX(pos.getX() + speed.getX());
+		pos.setY(pos.getY() + speed.getY());
+		setPosition(pos);
+	}
+	
+	public void setPosition(Vector v) {
+		collider.setPosition(v);
+		
+		int deltaX = (int)(v.getX() - collider.getPosition().getX());
+		int deltaY = (int)(v.getY() - collider.getPosition().getY());
+		polygon.translate(deltaX, deltaY);
+	}
+	
+	public Vector getPosition() {
+		return collider.getPosition();
+	}
+	
+
+}
